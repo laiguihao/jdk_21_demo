bb15ad7712412fe6f56a521a309d648f1b7fef73
shaoke <aaron.lai@shaoke.com>
2024-08-20 14:32:59 +0800

    项目初始化

---
+++ src/main/java/org/example/jdk_21_demo/JDk21DemoApplication.java
@@ -0,0 +1,13 @@
+package org.example.jdk_21_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JDk21DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JDk21DemoApplication.class, args);
+	}
+
+}
